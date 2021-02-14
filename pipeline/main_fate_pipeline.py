18ad0f4aa95bed078d52ccf4fd9ff992cc7f8c6c
Nikolai Kobyzev <kobyzev.n@gmail.com>
2021-02-14 18:29:00 +0300

    Появляется ошибка ValueError: num_label is 2

--- pipeline/main_fate_pipeline.py
+++ pipeline/main_fate_pipeline.py
@@ -5,13 +5,15 @@ from pipeline.component.dataio import DataIO
 from pipeline.component.reader import Reader
 from pipeline.interface.data import Data
 from pipeline.utils.tools import load_job_config
+from pipeline.runtime.entity import Evaluation
 from pipeline.runtime.entity import JobParameters
-from pipeline.runtime.entity import JobParameters
+from pipeline.component.homo_nn import HomoNN
+from tensorflow.keras import optimizers
+from tensorflow.keras.layers import Dense
 
 
-# noinspection PyPep8Naming
 class dataset(object):
-    breast = {
+    nsk_kdd = {
         "guest": {"name": "breast_homo_guest", "namespace": "experiment"},
         "host": [
             {"name": "breast_homo_host", "namespace": "experiment"},
@@ -27,9 +29,12 @@ class dataset(object):
     }
 
 
-def run_homo_nn_pipeline(config, namespace, data: dict, nn_component, num_host):
+def main(config="../../config.yaml", namespace="", ):
+    data = dataset.nsk_kdd
+
     if isinstance(config, str):
         config = load_job_config(config)
+    num_host = 2
 
     guest_train_data = data["guest"]
     host_train_data = data["host"][:num_host]
@@ -52,14 +57,24 @@ def run_homo_nn_pipeline(config, namespace, data: dict, nn_component, num_host):
         .component_param(with_label=True, output_format="dense")
     dataio_0.get_party_instance(role='host', party_id=hosts).component_param(with_label=True)
 
+    homo_nn_0 = HomoNN(name="homo_nn_0", encode_label=True, max_iter=15, batch_size=-1,
+                       early_stop={"early_stop": "diff", "eps": 0.0001})
+    homo_nn_0.add(Dense(units=5, input_shape=(18,), activation="relu"))
+    homo_nn_0.add(Dense(units=4, activation="sigmoid"))
+    homo_nn_0.compile(optimizer=optimizers.Adam(learning_rate=0.05), metrics=["accuracy"],
+                      loss="categorical_crossentropy")
+
+    evaluation_0 = Evaluation(name="evaluation_0", eval_type="multi")
+
     pipeline.add_component(reader_0)
     pipeline.add_component(dataio_0, data=Data(data=reader_0.output.data))
-    pipeline.add_component(nn_component, data=Data(train_data=dataio_0.output.data))
+    pipeline.add_component(homo_nn_0, data=Data(train_data=dataio_0.output.data))
+
     pipeline.compile()
     job_parameters = JobParameters(backend=config.backend, work_mode=config.work_mode)
     pipeline.fit(job_parameters)
     print(pipeline.get_component("homo_nn_0").get_summary())
-    pipeline.deploy_component([dataio_0, nn_component])
+    pipeline.deploy_component([dataio_0, homo_nn_0])
 
     # predict
     predict_pipeline = PipeLine()
@@ -71,12 +86,12 @@ def run_homo_nn_pipeline(config, namespace, data: dict, nn_component, num_host):
     predict_pipeline.predict(job_parameters)
 
 
-def runner(main_func):
+if __name__ == "__main__":
     parser = argparse.ArgumentParser("PIPELINE DEMO")
     parser.add_argument("-config", type=str,
                         help="config file")
     args = parser.parse_args()
     if args.config is not None:
-        main_func(args.config)
+        main(args.config)
     else:
-        main_func()
+        main()
