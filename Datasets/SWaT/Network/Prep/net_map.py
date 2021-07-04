56f368a97586b292b03856107ca13c5adc435807
Nikolai Kobyzev <kobyzev.n@gmail.com>
2021-07-04 23:39:58 +0300

    Уменьшенная ката сети

--- Datasets/SWaT/Network/Prep/net_map.py
+++ Datasets/SWaT/Network/Prep/net_map.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 filename = './net_map.csv'
 df = pd.read_csv(filename, header=0)
-G = nx.from_pandas_edgelist(df, 'src', 'dst', edge_attr=['num'], create_using=nx.MultiDiGraph())
-plt.figure(3, figsize=(60, 60))
-nx.draw_networkx(G, with_labels=True, node_size=7000, pos=nx.kamada_kawai_layout(G), arrowsize=20)
-plt.savefig('../maps/net_map_kawai.png')
-nx.draw(G, with_labels=True, node_size=7000, pos=nx.spring_layout(G), arrowsize=20)
-plt.savefig('../maps/net_map_spring.png')
-nx.draw(G, with_labels=True, node_size=7000, pos=nx.fruchterman_reingold_layout(G), arrowsize=20)
-plt.savefig('../maps/net_map_reingold.png')
+df['num'] = np.log10(df['num'])
+most_net = df.loc[df['num'] >= 3]
+G = nx.from_pandas_edgelist(most_net, 'src', 'dst', edge_attr=['num'], create_using=nx.MultiDiGraph())
+fig, ax = plt.subplots()
+fig.set_figheight(10)
+fig.set_figwidth(10)
+ax.set_title('Карта сети')
+nx.draw_networkx(G, ax=ax, with_labels=True, node_size=7000, width=most_net['num'].values.astype(float),
+                 pos=nx.kamada_kawai_layout(G), arrowsize=20)
+
+plt.savefig('../maps/net_map_kawai_more1000.png')
